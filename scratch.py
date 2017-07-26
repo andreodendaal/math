f76f918f0a87fa8efd2d90ec19a80500af960a05
U-GVWHOLDINGS\AOdendaal <AOdendaal@ACTY0090.gvwholdings.com>
2017-07-26 16:00:48 -0500

    Vector Class modifications

--- scratch.py
+++ scratch.py
@@ -1,36 +1,52 @@
 import numpy as np
+import vector
 
 
 def add_vectors(list_1, list_2):
     vector_1 = np.array(list_1)
     vector_2 = np.array(list_2)
     vector_sum = vector_1 + vector_2
-    return vector_sum
+    return str(vector_sum)
 
 def sub_vectors(list_1, list_2):
     vector_1 = np.array(list_1)
     vector_2 = np.array(list_2)
     vector_sum = vector_1 - vector_2
-    return vector_sum
+    return str(vector_sum)
 
 def multiply_vectors(list_1, mult):
     vector_1 = np.array(list_1)
     vector_prod = vector_1 * mult
-    return vector_prod
+    return str(vector_prod)
 
 
 q1a = [8.218, -9.341]
 q1b = [-1.129, 2.111]
+vector_1 = vector.Vector(q1a, q1b)
+answer_1_cls = vector_1.add_vectors()
+print('Dimension: ' + str(vector_1.dimension))
+print('Q1 Class:' + answer_1_cls)
+answer_1_fn = add_vectors(q1a, q1b)
+print('Q1 Function:' + answer_1_fn)
 
-answer_1 = add_vectors(q1a, q1b)
-print(answer_1)
 
 q2a = [7.119, 8.215]
 q2b = [-8.223, 0.878]
+vector_2 = vector.Vector(q2a, q2b)
+answer_2_fn = sub_vectors(q2a, q2b)
+print('Q2 Function answer: ' + str(answer_2_fn))
 
-answer_2 = sub_vectors(q2a, q2b)
-print(answer_2)
+answer_2_cls = vector_2.subtract_vectors()
+print('Q2 Class answer: ' + answer_2_cls)
 
 q3a = [1.671, -1.012, -0.318]
-answer_3 = multiply_vectors(q3a, 7.41)
-print(answer_3)
+answer_3_fn = multiply_vectors(q3a, 7.41)
+
+vector_3 = vector.Vector(q3a, [])
+answer_3_cls = vector_3.multiply_vectors(7.41)
+print("Q3 Function: " + answer_3_fn)
+print("Q3 class: " + answer_3_cls)
+
+print(answer_1_fn == answer_1_cls)
+print(answer_2_fn == answer_2_cls)
+print(answer_3_fn == answer_3_cls)
--- vector.py
+++ vector.py
@@ -1,10 +1,14 @@
+import numpy as np
+
+
 class Vector(object):
-    def __init__(self, coordinates):
+    def __init__(self, coordinate1, coordinate2):
         try:
-            if not coordinates:
+            if not coordinate1 and not coordinate2:
                 raise ValueError
-            self.coordinates = tuple(coordinates)
-            self.dimension = len(coordinates)
+            self.coordinate_1 = np.array(coordinate1)
+            self.coordinate_2 = np.array(coordinate2)
+            self.dimension = len(coordinate1) + len(coordinate2)
 
         except ValueError:
             raise ValueError('The coordinates must be nonempty')
@@ -12,10 +16,17 @@ class Vector(object):
         except TypeError:
             raise TypeError('The coordinates must be an iterable')
 
-
     def __str__(self):
-        return 'Vector: {}'.format(self.coordinates)
+        return 'Vector: {}'.format(self.coordinate_1 + self.coordinate_2)
 
+    def add_vectors(self):
+        vector_sum = self.coordinate_1 + self.coordinate_2
+        return str(vector_sum)
 
-    def __eq__(self, v):
-        return self.coordinates == v.coordinates
+    def multiply_vectors(self, mult):
+        vector_mult = self.coordinate_1 * mult
+        return str(vector_mult)
+
+    def subtract_vectors(self):
+        vector_sub = self.coordinate_1 - self.coordinate_2
+        return str(vector_sub)
